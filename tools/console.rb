c26ea2793bc0ef9ee558512b8b7d6c98d59ed7ea
Michael J Maggs <michaelmaggs@gmail.com>
2019-06-11 12:14:01 -0700

    failed

--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,19 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+michael = Customer.new("Michael", "Maggs")
+michelle = Customer.new("Michelle", "Maggs")
+emme = Customer.new("Emme", "Maggs")
+
+loulay = Restaurant.new("Loulay")
+cafe = Restaurant.new("Cafe Campagne")
+canlis = Restaurant.new("Canlis")
+
+review1 = Review.new(loulay, emme, "loved it!", 4)
+review2 = Review.new(canlis, emme, "never been", 0)
+review3 = Review.new(cafe, michael, "a Seattle staple", 4)
+review4 = Review.new(cafe, emme, "Calamari!", 3)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
